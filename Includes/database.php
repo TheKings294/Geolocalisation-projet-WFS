90b9c70f6659efc120b29e6361fc3d04dcf03f8b
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-01-21 22:38:42 +0100

    Ajout vriable env pour la BDD_NAME

--- Includes/database.php
+++ Includes/database.php
@@ -5,7 +5,7 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 try {
-    $pdo = new PDO('mysql:host='.$_ENV['BDD_URL'] . ';dbname=geoloc_projet', $_ENV['BDD_USERNAME'], $_ENV['BDD_PASSWORD']);
+    $pdo = new PDO('mysql:host='.$_ENV['BDD_URL'] . ';dbname=' . $_ENV['BDD_NAME'], $_ENV['BDD_USERNAME'], $_ENV['BDD_PASSWORD']);
 } catch (Exception $e) {
     $error[] = "BDD conect error : {$e->getMessage()}";
 }

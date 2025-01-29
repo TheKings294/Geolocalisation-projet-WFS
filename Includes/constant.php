a433834e07a2cc523ab5bd33b2e7c4cb3f8aa503
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-01-29 09:20:18 +0100

    Add home page of the projet

--- Includes/constant.php
+++ Includes/constant.php
@@ -1,5 +1,5 @@
 <?php
 const LIST_ITEM_PER_PAGE = 15;
-const UPLOAD_DIRECTORY = '/Geolocalisation-projet-WFS/uploads/';
+const UPLOAD_DIRECTORY = '/uploads/';
 const URL_SIRET = 'https://api.insee.fr/api-sirene/3.11/siret/';
 const URL_SIRENE = 'https://api.insee.fr/api-sirene/3.11/sirene/';

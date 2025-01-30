aac112c38ee41a4ad477c63f81f5ccbae577939a
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2025-01-30 15:52:50 +0100

    Ajout du port pour la co BDD

--- Includes/database.php
+++ Includes/database.php
@@ -7,7 +7,7 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 try {
-    $pdo = new PDO('mysql:host='.$_ENV['BDD_URL'] . ';dbname=' . $_ENV['BDD_NAME'], $_ENV['BDD_USERNAME'], $_ENV['BDD_PASSWORD']);
+    $pdo = new PDO('mysql:host='.$_ENV['BDD_URL'] . ';dbname=' . $_ENV['BDD_NAME'] . ';port=' . $_ENV['BDD_PORT'], $_ENV['BDD_USERNAME'], $_ENV['BDD_PASSWORD']);
 } catch (Exception $e) {
     $appLogger->alert('Database connection error: ' . $e->getMessage(), [
         'file' => __FILE__,
